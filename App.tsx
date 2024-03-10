49602d6b36d6ac2a7a68fa7975dc251355969357
GMP Sankalpa <malithapiumal02@gmail.com>
2024-03-10 18:12:20 +0530

    #06

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Button } from 'react-native';
 
 function App(): JSX.Element {
 
-  return(
-    <Text>Hello</Text>
+  return (
+    <View style={{
+      alignItems:'center',
+    }}>
+      <Text style={{
+        fontSize: 40,
+        color: 'red',
+        // marginLeft: 10,
+        marginTop: 10,
+      }} >
+        Hello
+      </Text>
+      <Text style={{
+        fontSize: 30,
+        // marginLeft: 40,
+        color: 'blue',
+        marginBottom:10
+      }}>
+        React native
+      </Text>
+      <Button title='Click me' />
+    </View>
   );
 }
 
